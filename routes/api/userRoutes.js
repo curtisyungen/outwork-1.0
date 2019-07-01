1b5668348db005952c18bb44d2b70ab5f58e6103
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-01 12:42:51 -0700

    Update userRoutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,16 +6,4 @@ router.get("/loginUser", (req, res) => {
     controller.loginUser(req, res);
 });
 
-router.get("/findExistingUser/:email", (req, res) => {
-    controller.findExistingUser(req, res);
-});
-
-router.post("/createNewUser", (req, res) => {
-    controller.createNewUser(req, res);
-});
-
-router.put("/submitNewPassword", (req, res) => {
-    controller.submitNewPassword(req, res);
-});
-
 module.exports = router;
