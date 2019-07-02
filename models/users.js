8e3ff9d4cdf5353f93019d5d05d3c7a25a03e2dc
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-01 20:09:04 -0700

    Add files via upload

---
+++ models/users.js
@@ -0,0 +1,22 @@
+module.exports = function(sequelize, DataTypes) {
+    let Users = sequelize.define("Users", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    });
+    
+    return Users;
+}
