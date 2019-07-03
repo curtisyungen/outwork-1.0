f1c017cd4af9c3d64136cf7bec66b77ec1360b20
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-03 10:01:40 -0700

    Add files via upload

---
+++ controllers/BikeController.js
@@ -0,0 +1,9 @@
+const db = require("../models/index.js");
+
+class BikeController {
+
+    
+
+}
+
+module.exports = BikeController;
---
+++ controllers/LiftController.js
@@ -0,0 +1,9 @@
+const db = require("../models/index.js");
+
+class LiftController {
+
+    
+
+}
+
+module.exports = LiftController;
