865080a057ab6daf44c766084ddfa7ce1a4ea88b
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-03 09:24:50 -0700

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -1,20 +1,39 @@
 module.exports = function(sequelize, DataTypes) {
     let Users = sequelize.define("Users", {
+        // Unique userID
+        userID: {
+            type: DataTypes.STRING,
+            allwNull: false,
+        },
+        // User's first name
         firstName: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        // User's last name
         lastName: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        // User's email address, used for login
         email: {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        // User's password, encrypted via bcrypt
         password: {
             type: DataTypes.STRING,
             allowNull: false,
+        }, 
+        // User's current weight in pounds, optional
+        weight: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+        },
+        // User's account privacy setting: public or private
+        privacy: {
+            type: DataTypes.STRING,
+            allowNull: true,
         }
     });
     
