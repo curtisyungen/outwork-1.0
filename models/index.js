3bc07e8be962dc171df6c1ec3a39f67bf8c21b93
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-03 14:15:59 -0700

    Add files via upload

--- models/index.js
+++ models/index.js
@@ -1,14 +1,30 @@
-var fs = require("fs");
+// File System
+var fs = require("fs"); 
+
+// Path
 var path = require("path");
-var Sequelize = require("sequelize");
+
+// ORM for communicating with MySQL
+var Sequelize = require("sequelize"); 
+
+// Identify basename of this file (index.js)
 var basename = path.basename(module.filename);
+
+// Identify environment being used or set to development
 var env = process.env.NODE_ENV || "development";
+
+// Load configuration based on environment
 var config = require(__dirname + "/../config/config.js")[env];
+
+// Initiatlize database as empty object
 var db = {};
 
+// If production, use production configuration
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
+} 
+// Otherwise use other configuration
+else {
   var sequelize = new Sequelize(
     config.database,
     config.username,
@@ -17,6 +33,10 @@ if (config.use_env_variable) {
   );
 }
 
+// Synchronous call to read file names from models directory
+// Filter through files in model directory
+// Select those that aren't index.js, .env, and that end with .js
+// Store models in database object with its name as key
 fs.readdirSync(__dirname)
   .filter(function(file) {
     return (
@@ -28,12 +48,14 @@ fs.readdirSync(__dirname)
     db[model.name] = model;
   });
 
+// Create associates between each model if present
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 
+// Store sequelize object and sequelize object reference in database object
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
