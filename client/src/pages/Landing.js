861056119dec1c10519b52a1c9e722eed9df2e09
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-03 18:13:46 -0700

    Add files via upload

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import "./Landing.css";
+
+class Landing extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+
+    render() {
+        return (
+
+            
+
+            <span>
+                <div class="jumbotron jumbotron-fluid text-center">
+                    <div>
+                        <h1 class="display-12">Outwork</h1>
+                        <p class="lead">Welcome.</p>
+                    </div>
+                    <div className="loginBtns btn-group col-lg-2 text-center">
+                        <button 
+                            className="btn btn-outline-dark btn-sm" 
+                            onClick={this.props.setRedirectToSignUp}
+                        >
+                            Sign Up
+                        </button>
+                        <button 
+                            className="btn btn-outline-dark btn-sm" 
+                            onClick={this.props.setRedirectToSignIn}
+                        >
+                            Sign In
+                        </button>
+                    </div>
+                </div>
+            </span>
+        )
+    }
+}
+
+export default Landing;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+// import Container from "../components/Container/container";
+import "./Login.css";
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        }
+    }
+
+    componentDidMount = () => {
+
+    }
+
+
+    render() {
+        return (
+            <></>
+        )
+    }
+}
+
+export default Home;
