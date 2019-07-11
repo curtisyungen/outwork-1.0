f8fcfa898a19754e3d43e12f8edecc149af06979
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-10 17:11:50 -0700

    Add files via upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import Container from "../components/Container/container";
-import userAPI from "../utils/userAPI";
-// import "./Profile.css";
+import Modal from "react-responsive-modal";
+import Run from "../components/Run/run";
+import Bike from "../components/Bike/bike";
+import Swim from "../components/Swim/swim";
+import Lift from "../components/Lift/lift";
+import actAPI from "../utils/actAPI";
+import "./Profile.css";
 
 class Profile extends Component {
 
@@ -14,34 +19,95 @@ class Profile extends Component {
             lastName: null,
             following: null,
             followers: null,
+            runs: null,
+            bikes: null,
+            swims: null,
+            lifts: null,
+            openFollowingModal: false,
+            openFollowersModal: false,
         }
     }
 
     componentDidMount = () => {
         this.props.checkValidUser();
 
-        let userId;
-        if (localStorage.getItem("userId") && localStorage.getItem("userId") !== null) {
-            userId = localStorage.getItem("userId");
-        }
-
+        let userId = localStorage.getItem("userId");
         this.setState({
             userId: userId,
         }, () => {
-            this.getUser();
+            this.getUserActivity();
+        });
+
+        
+    }
+
+    getUserActivity = () => {
+        actAPI.getRunsByUser(this.state.userId)
+            .then((res) => {
+                this.setState({
+                    runs: res.data,
+                });
+            });
+
+        actAPI.getBikesByUser(this.state.userId)
+            .then((res) => {
+                this.setState({
+                    bikes: res.data,
+                });
+            });
+
+        actAPI.getSwimsByUser(this.state.userId)
+            .then((res) => {
+                this.setState({
+                    swims: res.data,
+                });
+            });
+
+        actAPI.getLiftsByUser(this.state.userId)
+            .then((res) => {
+                this.setState({
+                    lifts: res.data,
+                });
+            });
+    }
+
+    deleteActivity = (type, id) => {
+
+        let userId = this.props.userId;
+
+        if (type === "run") {
+            actAPI.deleteRunById(id, userId);
+        }
+        else if (type === "bike") {
+            actAPI.deleteBikeById(id, userId);
+        }
+        else if (type === "swim") {
+            actAPI.deleteSwimById(id, userId);
+        }
+        else if (type === "lift") {
+            actAPI.deleteLiftById(id, userId);
+        }
+
+        window.location.reload();
+    }
+
+    openFollowingModal = () => {
+        this.setState({
+            openFollowingModal: true,
         });
     }
 
-    getUser = () => {
-        userAPI.getUserById(this.state.userId)
-            .then((res) => {
-                this.setState({
-                    firstName: res.data[0].firstName,
-                    lastName: res.data[0].lastName,
-                    following: res.data[0].following,
-                    followers: res.data[0].followers,
-                });
-            });
+    openFollowersModal = () => {
+        this.setState({
+            openFollowersModal: true,
+        });
+    }
+
+    closeModal = () => {
+        this.setState({
+            openFollowingModal: false,
+            openFollowersModal: false,
+        });
     }
 
     render() {
@@ -49,11 +115,129 @@ class Profile extends Component {
             <Container>
                 <div className="profile">
                     <div>
-                        <h4>{this.state.firstName} {this.state.lastName}</h4>
+                        <h4>{this.props.firstName} {this.props.lastName}</h4>
                         <div>
-                            <p>Following: {this.state.following ? (this.state.following.length):("")}</p>
-                            <p>Followers: {this.state.followers ? (this.state.followers.length):("")}</p>
+                            <div
+                                className="followStats"
+                                onClick={this.openFollowingModal}
+                            >
+                                Following: {this.props.following ? (this.props.following.length) : ("")}
+                            </div>
+
+                            <div
+                                className="followStats"
+                                onClick={this.openFollowersModal}
+                            >
+                                Followers: {this.props.followers ? (this.props.followers.length) : ("")}
+                            </div>
                         </div>
+                    </div>
+
+                    <Modal
+                        open={this.state.openFollowingModal}
+                        onClose={this.closeModal}
+                    >
+
+                    </Modal>
+
+                    <Modal
+                        open={this.state.openFollowersModal}
+                        onClose={this.closeModal}
+                    >
+
+                    </Modal>
+
+                    <div>
+                        {/* RUNS */}
+                        {this.state.runs && this.state.runs.length ? (
+                            this.state.runs.map(run => (
+                                <Run
+                                    key={run.id}
+                                    id={run.id}
+                                    firstName={run.firstName}
+                                    lastName={run.lastName}
+                                    date={run.date}
+                                    distance={run.distance}
+                                    duration={run.duration}
+                                    milePace={run.milePace}
+                                    type={run.type}
+                                    repeats={run.repeats}
+                                    race={run.race}
+                                    location={run.location}
+                                    surface={run.surface}
+                                    weather={run.weather}
+                                    climb={run.climb}
+                                    grade={run.grade}
+                                    shoe={run.shoe}
+                                    notes={run.notes}
+                                    map={run.map}
+                                    deleteActivity={this.deleteActivity}
+                                />
+                            ))
+                        ) : (
+                                <></>
+                            )}
+
+                        {/* BIKES */}
+                        {this.state.bikes && this.state.bikes.length ? (
+                            this.state.bikes.map(bike => (
+                                <Bike
+                                    key={bike.id}
+                                    id={bike.id}
+                                    firstName={bike.firstName}
+                                    lastName={bike.lastName}
+                                    date={bike.date}
+                                    distance={bike.distance}
+                                    duration={bike.duration}
+                                    deleteActivity={this.deleteActivity}
+                                />
+                            ))
+                        ) : (
+                                <></>
+                            )}
+
+                        {/* SWIMS */}
+                        {this.state.swims && this.state.swims.length ? (
+                            this.state.swims.map(swim => (
+                                <Swim
+                                    key={swim.id}
+                                    id={swim.id}
+                                    firstName={swim.firstName}
+                                    lastName={swim.lastName}
+                                    date={swim.date}
+                                    distance={swim.distance}
+                                    duration={swim.duration}
+                                    deleteActivity={this.deleteActivity}
+                                />
+                            ))
+                        ) : (
+                                <></>
+                            )}
+
+                        {/* LIFTS */}
+                        {this.state.lifts && this.state.lifts.length ? (
+                            this.state.lifts.map(lift => (
+                                <Lift
+                                    key={lift.id}
+                                    id={lift.id}
+                                    userId={lift.userId}
+                                    firstName={lift.firstName}
+                                    lastName={lift.lastName}
+                                    date={lift.date}
+                                    duration={lift.duration}
+                                    generator={lift.generator}
+                                    location={lift.location}
+                                    muscleGroups={lift.muscleGroups}
+                                    notes={lift.notes}
+                                    pullups={lift.pullups}
+                                    pushups={lift.pushups}
+                                    workout={lift.workout}
+                                    deleteActivity={this.deleteActivity}
+                                />
+                            ))
+                        ) : (
+                                <></>
+                            )}
                     </div>
                 </div>
             </Container>
