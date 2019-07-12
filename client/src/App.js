f31754d4058a2b586025455c3cd4f2385e936920
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-12 07:10:41 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -397,14 +397,14 @@ class App extends Component {
               />
             } />
 
-            <Route path="/profile/:userId" render={() => {
+            <Route path="/profile/:userId" render={() => 
               <Profile
                 checkValidUser={this.checkValidUser}
                 getUserActivity={this.getUserActivity}
                 allActivity={this.state.allActivity}
                 deleteActivity={this.deleteActivity}
               />
-            }} />
+            } />
 
             {/* Log Activity Page */}
             <Route exact path="/logActivity" render={() =>
