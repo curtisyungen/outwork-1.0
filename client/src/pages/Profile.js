58b26ad46022af17cdcd24a25777157ee0a7be27
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-12 06:29:55 -0700

    Add files via upload

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -5,20 +5,51 @@ import "./Profile.css";
 
 class Profile extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userId: null,
+            firstName: null,
+            lastName: null,
+        }
+    }
+
     componentDidMount = () => {
         this.props.checkValidUser();
-    
-        let userId;
-        userId = localStorage.getItem("userId");
 
-        this.props.getUserActivity(userId);
+        let userId, firstName, lastName;
+        
+        if (this.props.location.state && this.props.location.state.userId !== null) {
+            userId = this.props.location.state.userId;
+            firstName = this.props.location.state.firstName;
+            lastName = this.props.location.state.lastName;
+        }
+        else {
+            userId = localStorage.getItem("userId");
+            firstName = localStorage.getItem("fn");
+            lastName = localStorage.getItem("ln");
+        }
+    
+        this.setState({
+            userId: userId,
+            firstName: firstName,
+            lastName: lastName,
+        }, () => {
+            this.props.getUserActivity(userId);
+        });
     }
 
     render() {
         return (
             <span>
-                <ProfileHeader />
+                <ProfileHeader 
+                    userId={this.state.userId}
+                    firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                />
                 <ProfileBody 
+                    userId={this.state.userId}
                     allActivity={this.props.allActivity}
                     deleteActivity={this.props.deleteActivity}
                 />                    
