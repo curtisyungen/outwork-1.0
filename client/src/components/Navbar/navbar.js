ebd2a1d7e8c94be26f92cb3d7a56eaca41db7a46
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-12 06:38:18 -0700

    Add files via upload

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -13,7 +13,7 @@ class Navbar extends Component {
                     "/profile", 
                     "/logactivity", 
                     "/generator",
-                    "/findUsers",
+                    "/findusers",
                     "/settings",
                     "/run",
                     "/bike",
