e17bd397460f3e48b958a9762c2dec341dc67b43
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-12 23:43:45 -0700

    Add files via upload

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,27 +1,15 @@
 const path = require("path");
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
-const runRoutes = require("./runRoutes");
-const bikeRoutes = require("./bikeRoutes");
-const swimRoutes = require("./swimRoutes");
-const liftRoutes = require("./liftRoutes");
 const exerciseRoutes = require("./exerciseRoutes");
+const workoutRoutes = require("./workoutRoutes");
 const resetRoutes = require("./resetRoutes");
 
 // User routes
 router.use("/users", userRoutes);
 
-// Run routes
-router.use("/runs", runRoutes);
-
-// Bike routes
-router.use("/bikes", bikeRoutes);
-
-// Swim routes
-router.use("/swims", swimRoutes);
-
-// Lift routes
-router.use("/lifts", liftRoutes);
+// Workout routes
+router.use("/workouts", workoutRoutes);
 
 // Exercise routes
 router.use("/exercises", exerciseRoutes);
---
+++ routes/api/workoutRoutes.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const WorkoutController = require("../../controllers/WorkoutController");
+const controller = new WorkoutController();
+
+router.get("/getAllWorkoutsByUserId/:userId", (req, res) => {
+    controller.getAllWorkoutsByUserId(req, res);
+});
+
+router.get("/getAllWorkouts", (req, res) => {
+    controller.getAllWorkouts(req, res);
+});
+
+router.post("/createWorkout", (req, res) => {
+    controller.createWorkout(req, res);
+});
+
+router.delete("/deleteWorkoutById", (req, res) => {
+    controller.deleteWorkoutById(req, res);
+});
+
+module.exports = router;
