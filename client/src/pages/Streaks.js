160e3f6c82ba7a66f9929c83f46f00843d4559a0
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-17 05:00:30 -0700

    Add files via upload

--- client/src/pages/Streaks.js
+++ client/src/pages/Streaks.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import BarChart from "../components/BarChart/barChart";
+import workoutAPI from "../utils/workoutAPI";
 import "./Streaks.css";
 
 let d3 = require("d3");
@@ -10,20 +11,50 @@ class Streaks extends Component {
         super(props);
 
         this.state = {
-            dataset: [1, 2, 3],
+            userId: null,
+            dataset: [],
         }
     }
 
     componentDidMount = () => {
+        let userId = localStorage.getItem("userId");
 
+        this.setState({
+            userId: userId,
+        }, () => {
+            this.getUserData();
+        });
+    }
+
+    getUserData = () => {
+        workoutAPI.getAllWorkoutsByUserId(this.state.userId)
+            .then((res) => {
+                let workouts = res.data;
+                let dataset = [];
+
+                for (var w in workouts) {
+                    if (workouts[w].workoutType === "run") {
+                        dataset.push(parseFloat(workouts[w].distance));
+                    }
+                }
+
+                this.setState({
+                    dataset: dataset,
+                });
+            });
     }
 
     render() {
         return (
-            <BarChart 
+            this.state.dataset.length > 0 ? (
+                <BarChart 
                 data={this.state.dataset}
                 size={[500, 500]}
             />
+            ) : (
+                <></>
+            )
+            
         )
     }
 }
