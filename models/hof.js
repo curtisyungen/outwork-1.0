bae2384a78560f3c2d0ac8b4022d54aa69781c31
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-30 15:35:33 -0700

    Add files via upload

--- models/hof.js
+++ models/hof.js
@@ -1,7 +1,27 @@
 module.exports = function(sequelize, DataTypes) {
     let Hof = sequelize.define("Hof", {
-        // Name of award
+        // Name reference to award (mostWorkouts, mostRestDays, etc.)
         award: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Title of Award (Hardest Worker, Snoozer, etc.)
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Description of award
+        desc: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Font Awesome icon reference
+        icon: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Units for measuring award value (workouts, days, feet, etc.)
+        unit: {
             type: DataTypes.STRING,
             allowNull: false,
         },
