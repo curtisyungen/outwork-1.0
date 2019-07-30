c752e59ff200aac9203ab9b3970f2a60dcbbb34d
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-30 06:14:12 -0700

    Add files via upload

--- client/src/pages/HallOfFame.js
+++ client/src/pages/HallOfFame.js
@@ -23,7 +23,10 @@ class HallOfFame extends Component {
         super(props);
 
         this.state = {
-
+            userId: null,
+            firstName: null,
+            lastName: null,
+            awards: null,
         }
     }
 
@@ -56,7 +59,15 @@ class HallOfFame extends Component {
     getHof = () => {
         hofAPI.getHof()
             .then((res) => {
-                console.log(res);
+                let awards = [];
+
+                for (var r in res.data) {
+                    awards.push([res.data[r].firstName, res.data[r].value]);
+                }
+
+                this.setState({
+                    awards: awards,
+                });
             });
     }
 
@@ -66,126 +77,23 @@ class HallOfFame extends Component {
     render() {
         return (
             <div className="container pageContainer hofContainer">
-                {this.state.mostWorkouts && this.state.mostRestDays && this.state.longestRun && this.state.maxClimb && this.state.mostPushUps &&
-                    this.state.mostPullUps && this.state.mostGoggins && this.state.mostRainyDays && this.state.mostSwims && this.state.mostRaces
-                    ? (
-                        <span>
-                            <h4>Hall of Fame</h4>
-                            <div className="hallOfFame">
-                                {/* MOST WORKOUTS */}
-                                <div className="hofMetric mostWorkouts">
-                                    <div className="hofHover">Highest number of workouts completed of any type.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x trophyIcon" icon={faTrophy} /></div>
-                                    <div className="hofTitle">Hardest Worker</div>
-                                    <div className="hofName">{this.state.mostWorkouts[1]}</div>
-                                    <div className="hofValue">{this.state.mostWorkouts[0]} workouts</div>
-                                </div>
-
-                                {/* MOST REST DAYS */}
-                                <div className="hofMetric mostRestDays">
-                                    <div className="hofHover">Most number of rest days. Person here needs to stop cuddling with blanky, get the fuck out of bed, and quit being a lazy bitch.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x restIcon" icon={faBed} /></div>
-                                    <div className="hofTitle">Snoozer</div>
-                                    <div className="hofName">{this.state.mostRestDays[1]}</div>
-                                    <div className="hofValue">{this.state.mostRestDays[0]} rest days</div>
-                                </div>
-
-                                {/* LONGEST RUN */}
-                                <div className="hofMetric longestRun">
-                                    <div className="hofHover">Most number of miles run in one workout.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x rulerIcon" icon={faRulerHorizontal} /></div>
-                                    <div className="hofTitle">Endurer</div>
-                                    <div className="hofName">{this.state.longestRun.firstName}</div>
-                                    <div className="hofValue">{this.state.longestRun.distance} miles</div>
-                                </div>
-
-                                {/* GREATEST CLIMB */}
-                                <div className="hofMetric mostClimb">
-                                    <div className="hofHover">Most elevation climbed in single workout.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x mountainIcon" icon={faMountain} /></div>
-                                    <div className="hofTitle">Climber</div>
-                                    <div className="hofName">{this.state.maxClimb.firstName}</div>
-                                    <div className="hofValue">{this.state.maxClimb.climb} feet</div>
-                                </div>
-
-                                {/* MOST PUSH-UPS */}
-                                <div className="hofMetric mostPushUps">
-                                    <div className="hofHover">Most push-ups done in a single workout.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x silverMedalIcon" icon={faMedal} /></div>
-                                    <div className="hofTitle">Push-Up Bro</div>
-                                    <div className="hofName">{this.state.mostPushUps.firstName}</div>
-                                    <div className="hofValue">{this.state.mostPushUps.pushups} push-ups</div>
-                                </div>
-
-                                {/* MOST PULL-UPS */}
-                                <div className="hofMetric mostPullUps">
-                                    <div className="hofHover">Most pull-ups done in a single workout.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x bronzeMedalIcon" icon={faMedal} /></div>
-                                    <div className="hofTitle">Prince of Pulls</div>
-                                    <div className="hofName">{this.state.mostPullUps.firstName}</div>
-                                    <div className="hofValue">{this.state.mostPullUps.pullups} pull-ups</div>
-                                </div>
-
-                                {/* MOST GOGGINS WORKOUTS */}
-                                <div className="hofMetric mostGoggins">
-                                    <div className="hofHover">Most Goggins workouts completed. Winner here is a total badass.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x dragonIcon" icon={faDragon} /></div>
-                                    <div className="hofTitle">Most Goggins</div>
-                                    <div className="hofName">{this.state.mostGoggins[1]}</div>
-                                    <div className="hofValue">{this.state.mostGoggins[0]} workouts</div>
-                                </div>
-
-                                {/* MOST RACES */}
-                                <div className="hofMetric mostRaces">
-                                    <div className="hofHover">Most number of races completed.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x flagIcon" icon={faFlagCheckered} /></div>
-                                    <div className="hofTitle">Competitor</div>
-                                    <div className="hofName">{this.state.mostRaces[1]}</div>
-                                    <div className="hofValue">{this.state.mostRaces[0]} races</div>
-                                </div>
-
-                                {/* MOST TIME */}
-                                <div className="hofMetric mostTime">
-                                    <div className="hofHover">Most time spent working out. Measured in minutes.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x clockIcon" icon={faClock} /></div>
-                                    <div className="hofTitle">Most Time</div>
-                                    <div className="hofName">{null}</div>
-                                    <div className="hofValue">{null} minutes</div>
-                                </div>
-
-                                {/* MOST RAINY DAYS */}
-                                <div className="hofMetric mostRainyDays">
-                                    <div className="hofHover">Most days run in heavy rain.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x rainIcon" icon={faCloudShowersHeavy} /></div>
-                                    <div className="hofTitle">Rain Man</div>
-                                    <div className="hofName">{this.state.mostRainyDays[1]}</div>
-                                    <div className="hofValue">{this.state.mostRainyDays[0]} rain runs</div>
-                                </div>
-
-                                {/* MOST SWIMS */}
-                                <div className="hofMetric mostSwims">
-                                    <div className="hofHover">Most swim workouts completed.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x fishIcon" icon={faFish} /></div>
-                                    <div className="hofTitle">Merman</div>
-                                    <div className="hofName">{this.state.mostSwims[1]}</div>
-                                    <div className="hofValue">{this.state.mostSwims[0]} swims</div>
-                                </div>
-
-                                {/* HOT DOG */}
-                                <div className="hofMetric hotdog">
-                                    <div className="hofHover">The coveted weiner award.</div>
-                                    <div className="hofIcon"><FontAwesomeIcon className="fa-3x hotdogIcon" icon={faHotdog} /></div>
-                                    <div className="hofTitle">Weiner</div>
-                                    <div className="hofName">{this.state.hotdog[1]}</div>
-                                    <div className="hofValue">{this.state.hotdog[0]}</div>
-                                </div>
+                <h4>Hall of Fame</h4>
+                <div className="hallOfFame">
+                    {this.state.awards && this.state.awards.length > 0 ? (
+                        this.state.awards.map(award => (
+                            <div className="hofMetric mostWorkouts">
+                                <div className="hofHover">Description</div>
+                                <div className="hofIcon"><FontAwesomeIcon className="fa-3x trophyIcon" icon={faTrophy} /></div>
+                                <div className="hofTitle">Award Title</div>
+                                <div className="hofName">{award.userName}</div>
+                                <div className="hofValue">{award.value} units</div>
                             </div>
-                        </span>
+                        ))
                     ) : (
                         <></>
                     )}
+                </div>
             </div>
-
         )
     }
 }
