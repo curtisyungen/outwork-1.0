dc3aa0c8d00f4b884891ecf11f93de27b69d2b92
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-07-30 20:23:15 -0700

    Add files via upload

--- client/src/components/Day/day.js
+++ client/src/components/Day/day.js
@@ -9,45 +9,46 @@ class Day extends Component {
 
         this.state = {
             type: null,
-            workoutType: null,
+            workoutTypes: null,
         }
     }
 
     componentDidMount = () => {
-        this.getType();
         this.getWorkoutData();
     }
 
-    getType = () => {
+    getWorkoutData = () => {
         let type;
         let day = this.props.day;
 
+        // If day has multiple workouts, set type as multiple for color scheme
         if (day.length > 2) {
             type = "multiple";
         }
+        // Otherwise, set type as the type of workout for that day
         else if (day.length === 2) {
             type = day[1].workoutType;
         }
 
-        this.setState({
-            type: type,
-        });
-    }
-
-    getWorkoutData = () => {
-        let day = this.props.day;
-        let workoutType = [];
+        // Gather all workout types for the day to be shown in PopUp
+        let workoutTypes = [];
 
         if (day.length >= 2) {
             for (var i=1; i<day.length; i++) {
-                workoutType.push(day[i].workoutType);
+
+                if (day[i].workoutType !== "lift") {
+                    workoutTypes.push([day[i].workoutType, `${day[i].distance} miles`]);
+                }
+                else {
+                    workoutTypes.push([day[i].workoutType, `${day[i].generator}`]);
+                }
             }
         }
 
         this.setState({
-            workoutType: workoutType,
+            type: type,
+            workoutTypes: workoutTypes,
         });
-
     }
 
     render() {
@@ -62,11 +63,13 @@ class Day extends Component {
                     closeOnDocumentClick
                     className="popup"
                 >
-                    {this.state.workoutType && this.state.workoutType.length > 0 ? (
+                    {this.state.workoutTypes && this.state.workoutTypes.length > 0 ? (
                         <span>
                             <p>{`${this.props.month} ${parseInt(this.props.day[0])}, 2019`}</p>
-                            {this.state.workoutType.map(type => (
-                                <div>{type.toUpperCase()}</div>
+                            {this.state.workoutTypes.map((type) => (
+                                <div key={Math.random() * 100000}>
+                                    <span>{type[0].toUpperCase()}:&nbsp;{type[1]}</span>
+                                </div>
                             ))}
                         </span>
                     ) : (
