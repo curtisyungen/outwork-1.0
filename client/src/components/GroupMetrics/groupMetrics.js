10f08c17b9f64cf70e24fecd5b509ad3a24d0dc3
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-02 19:27:29 -0700

    Add files via upload

--- client/src/components/GroupMetrics/groupMetrics.js
+++ client/src/components/GroupMetrics/groupMetrics.js
@@ -5,14 +5,13 @@ import userAPI from "../../utils/userAPI";
 import "./groupMetrics.css";
 
 class GroupMetrics extends Component {
-    
+
     constructor(props) {
         super(props);
 
         this.state = {
             groupMetrics: [],
             users: null,
-            workouts: null,
         }
     }
 
@@ -26,124 +25,95 @@ class GroupMetrics extends Component {
                 this.setState({
                     users: res.data,
                 }, () => {
-                    this.getMetrics();
-                });
-            })
-    }
-
-    getMetrics = () => {
-        let users = this.state.users;
-
-        for (var u in users) {
-            this.getWorkouts(users[u].userId);
-        }
-    }
-
-    getWorkouts = (userId) => {
-        workoutAPI.getAllWorkoutsByUserId(userId)
-            .then((res) => {
-                this.setState({
-                    workouts: res.data,
-                }, () => {
-                    this.analyzeWorkouts(userId);
+                    let users = res.data;
+                    for (var u in users) {
+                        this.getMetrics(users[u].userId, users[u].firstName);
+                    }
                 });
             });
     }
 
-    analyzeWorkouts = (userId) => {
-        let user = {
-            name: null,
-            workouts: 0,
-            currWkMiles: 0,
-            avgMiles: 0,
-            maxMiles: 0,
-            avgDist: 0,
-            longestRun: 0,
-            avgPace: 0,
-            totalMiles: 0,
-            distClimb: 0,
-            pushUps: 0,
-            pullUps: 0,
-        }
+    getMetrics = (userId, firstName) => {
+        let groupMetrics = this.state.groupMetrics;
 
-        let users = this.state.users;
+        let userMetrics = {};
 
-        for (var u in users) {
-            if (users[u].userId === userId) {
-                user.name = users[u].firstName;
-            }
-        }
+        workoutAPI.getAllWorkoutsByUserId(userId)
+            .then((res) => {
+                let workouts = res.data;
+                let pushups = 0, pullups = 0, climb = 0, miles = 0, currWkMiles = 0;
 
-        let workouts = this.state.workouts;
+                for (var w in workouts) {
 
-        for (var w in workouts) {
-            if (workouts[w].userId === userId) {
-                user.workouts += 1;
+                    // Get miles run, climb
+                    if (workouts[w].workoutType === "run") {
+                        miles += parseFloat(workouts[w].distance);
+                        climb += parseFloat(workouts[w].climb);
+                    }
 
-                if (workouts[w].workoutType === "run") {
-                    user.totalMiles += parseFloat(workouts[w].distance);
-                    user.distClimb += parseFloat(workouts[w].climb);
+                    // Get pushups
+                    if (workouts[w].pushups !== null) {
+                        pushups += parseFloat(workouts[w].pushups);
+                    }
 
-                    if (workouts[w].distance > user.longestRun) {
-                        user.longestRun = parseFloat(workouts[w].distance);
+                    // Get pullups
+                    if (workouts[w].pullups !== null) {
+                        pullups += parseFloat(workouts[w].pullups);
                     }
                 }
 
-            }
-        }
+                userMetrics.firstName = firstName;
+                userMetrics.workouts = workouts.length;
+                userMetrics.currWkMiles = 0;
+                userMetrics.totalMilesRun = miles;
+                userMetrics.distClimbed = climb;
+                userMetrics.pushups = pushups;
+                userMetrics.pullups = pullups;
 
-        let groupMetrics = this.state.groupMetrics;
-        groupMetrics.push(user);
+                groupMetrics.push(userMetrics);
 
-        this.setState({
-            groupMetrics: groupMetrics,
-        });
+                this.setState({
+                    groupMetrics: groupMetrics,
+                });
+            });
     }
 
     render() {
         return (
-            <div className="groupMetrics">
-                <div className="gm-row">
-                    <div className="gm-col">
-                        <div className="gm-cell">User Name</div>
-                        <div className="gm-cell">Total Workouts</div>
-                        <div className="gm-cell">Current Week's Miles</div>
-                        <div className="gm-cell">Avg. Miles / Week</div>
-                        <div className="gm-cell">Max. Miles / Week</div>
-                        <div className="gm-cell">Avg. Distance Run (mi.)</div>
-                        <div className="gm-cell">Longest Run (mi.)</div>
-                        <div className="gm-cell">Avg. Mile Pace</div>
-                        <div className="gm-cell">Total Miles Run</div>
-                        <div className="gm-cell">Distance Climbed (ft.)</div>
-                        <div className="gm-cell">Push-Ups</div>
-                        <div className="gm-cell">Pull-Ups</div>
-                    </div>
-                </div>
-
+            <span>
+                <h4>Quick Stats</h4>
                 {this.state.groupMetrics && this.state.groupMetrics.length > 0 ? (
-                    this.state.groupMetrics.map(indiv => (
-                        <div key={indiv.name} className="gm-row">
+                    <div className="groupMetrics">
+                        <div className="gm-row">
                             <div className="gm-col">
-                                <div className="gm-cell">{indiv.name}</div>
-                                <div className="gm-cell">{indiv.workouts}</div>
-                                <div className="gm-cell">{indiv.currWkMiles}</div>
-                                <div className="gm-cell">{indiv.avgMiles}</div>
-                                <div className="gm-cell">{indiv.maxMiles}</div>
-                                <div className="gm-cell">{indiv.avgDist}</div>
-                                <div className="gm-cell">{indiv.longestRun}</div>
-                                <div className="gm-cell">{indiv.avgPace}</div>
-                                <div className="gm-cell">{indiv.totalMiles}</div>
-                                <div className="gm-cell">{indiv.distClimb}</div>
-                                <div className="gm-cell">{indiv.pushUps}</div>
-                                <div className="gm-cell">{indiv.pullUps}</div>
+                                <div className="gm-cell">User Name</div>
+                                <div className="gm-cell">Total Workouts</div>
+                                <div className="gm-cell">Current Week's Miles</div>
+                                <div className="gm-cell">Total Miles Run</div>
+                                <div className="gm-cell">Distance Climbed (ft.)</div>
+                                <div className="gm-cell">Push-Ups</div>
+                                <div className="gm-cell">Pull-Ups</div>
                             </div>
                         </div>
-                    ))
-                ) : (
-                    <></>
-                )}
 
-            </div>
+                        {this.state.groupMetrics.map(indiv => (
+                            <div key={indiv.name} className="gm-row">
+                                <div className="gm-col">
+                                    <div className="gm-cell">{indiv.firstName}</div>
+                                    <div className="gm-cell">{indiv.workouts}</div>
+                                    <div className="gm-cell">{indiv.currWkMiles}</div>
+                                    <div className="gm-cell">{indiv.totalMilesRun}</div>
+                                    <div className="gm-cell">{indiv.distClimbed}</div>
+                                    <div className="gm-cell">{indiv.pushups}</div>
+                                    <div className="gm-cell">{indiv.pullups}</div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>Loading stats...</p>
+                )}
+            </span>
         )
     }
 }
