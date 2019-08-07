a5ac336d3776a803c27c51d971d6ea04b9b9c77c
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-07 07:19:29 -0700

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -554,25 +554,6 @@ class App extends Component {
     });
   }
 
-  calcTtlMinutes = () => {
-    workoutAPI.getAllWorkouts()
-      .then((res) => {
-        let workouts = res.data;
-        let ttlMins;
-        
-        for (var w in workouts) {
-          let time = workouts[w].duration;
-          let hours = parseFloat(time.split(":")[0]);
-          let mins = parseFloat(time.split(":")[1]);
-          let secs = parseFloat(time.split(":")[2]);
-          
-          ttlMins = (hours * 60) + mins + (secs / 60);
-
-          workoutAPI.setTtlMins(workouts[w].id, ttlMins);
-        }
-      });
-  }
-
   render() {
     return (
       <Router>
@@ -714,6 +695,7 @@ class App extends Component {
                 updateParentState={this.updateParentState}
                 checkValidUser={this.checkValidUser}
                 getAllWorkouts={this.getAllWorkouts}
+                getRecentWorkouts={this.getRecentWorkouts}
                 allActivity={this.state.allActivity}
                 deleteActivity={this.deleteActivity}
                 background={this.state.background}
