6c030c875282a9d27cf8e4f86472f4adf5546625
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-10 17:59:50 -0700

    Add files via upload

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -24,6 +24,7 @@ class Navbar extends Component {
                     "/halloffame",
                     "/allusers",
                     "/run",
+                    "/updaterun",
                     "/bike",
                     "/swim",
                     "/lift",
