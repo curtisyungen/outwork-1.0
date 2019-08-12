57c7fa5fc9ab9fac11bc361d3717131d7215d051
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-12 06:25:13 -0700

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -219,7 +219,7 @@ class Home extends Component {
                     
                     <div className="activityIcons-home">
                         <div className="welcomeNote">
-                            <h4>Welcome, {this.state.firstName}</h4>
+                            <h4>Welcome, {this.state.firstName}.</h4>
                         </div>
             
                         <ActivityIcons />
