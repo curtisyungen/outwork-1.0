02979f0a4711b5d8a9322d0fb4ce17718e0265b4
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-13 17:17:07 -0700

    Add files via upload

---
+++ client/src/pages/Faq.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./Faq.css";
+
+class Faq extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            users: null,
+        }
+    }
+
+    componentDidMount = () => {
+        this.props.checkValidUser();
+
+    }
+
+    render() {
+        return (
+            <div className="container pageContainer faqContainer">
+                <div className="questions">
+                    <h4>Frequently Asked Questions</h4>
+                    <div className="qaDiv">
+                        <div className="q">Q: How do you win the weiner award?</div>
+                        <div className="a">A: Trade secret. Rest assured that everyone has an equal opportunity to win the coveted Weiner Award!</div>
+                    </div>
+
+                    <div className="qaDiv">
+                        <div className="q">Q: How is the Champ of the Week award determined?</div>
+                        <div className="a">
+                            A: It's based on weekly stats for most workouts, most push-ups, most pull-ups,
+                            most time spent working out, and most elevation climbed during runs.
+                        </div>
+                    </div>
+
+                    <div className="qaDiv">
+                        <div className="q">Q: What was the inspiration behind the name Outwork?</div>
+                        <div className="a">
+                            A: It's an obvious play on the word "workout". In fact, the name itself inspired the Hall of Fame,
+                            which aims to inspire users to outwork each other or simply outwork their earlier selves.
+                        </div>
+                    </div>
+                </div>
+
+                <div className="inWork">
+                    <h4>Outwork In Work</h4>
+                    <div className="iw">08/13/2019: Adding bar chart for visualizing weekly running mileage.</div>
+                    <div className="iw">08/13/2019: Fixing push-up count on Generator workout submissions.</div>
+                    <div className="iw">08/13/2019: Making muscle group names consistent between Lift and Generator submissions ("legs" vs. "quadriceps").</div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Faq;
