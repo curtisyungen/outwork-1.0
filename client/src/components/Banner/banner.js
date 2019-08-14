21d7a43ab1e683afd0096ee42261f5454a7c8421
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2019-08-13 17:46:18 -0700

    Add files via upload

---
+++ client/src/components/Banner/banner.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./banner.css";
+
+class Banner extends Component {
+    render() {
+        return (
+            <div className="banner">
+                Heads up: current glitch with Goggins generator workout submissions. Push-ups aren't being counted. Many reps left blank. Currently investigating.
+            </div>
+        )
+    }
+}
+
+export default Banner;
